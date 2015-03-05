11f6e33f8f8bedc9b84133eece0c52f36508da1b
vitex <vitex@linuxadmin.austro-bohemia.local>
2015-03-05 16:01:39 +0100

    Doplněna TWBTextarea

--- EaseTWBootstrap.php
+++ EaseTWBootstrap.php
@@ -1107,3 +1107,31 @@ class EaseTWBLabel extends EaseHtmlSpanTag
     }
 
 }
+
+/**
+ * Textarea pro Twitter Bootstap
+ *
+ * @author vitex
+ */
+class EaseTWBTextarea extends EaseHtmlTextareaTag
+{
+
+    /**
+     * Textarea
+     *
+     * @param string $name       jméno tagu
+     * @param string $content    obsah textarey
+     * @param array  $properties vlastnosti tagu
+     */
+    public function __construct($name, $content = '', $properties = null)
+    {
+        if (is_null($properties) || !isset($properties['class'])) {
+            $properties['class'] = 'form-control';
+        } else {
+            $properties['class'] .= ' form-control';
+        }
+
+        parent::__construct($name, $content, $properties);
+    }
+
+}
