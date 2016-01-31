b64cc36e2272872a521e0d5e41a07050a04eb10c
V?t?zslav Dvo??k <info@vitexsoftware.cz>
2016-02-01 00:56:07 +0100

    Added TWB\Carousel

---
+++ src/Ease/TWB/Carousel.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * Twitter Bootrstap Container
+ * 
+ * @package   EaseFrameWork
+ * @author    Vitex <vitex@hippy.cz>
+ * @copyright 2009-2016 Vitex@hippy.cz (G)
+ */
+
+namespace Ease\TWB;
+
+/**
+ * Carousel for Twitter Bootstrap
+ */
+class Carousel extends \Ease\Html\Div {
+
+    /**
+     * Carousel name
+     * @var string 
+     */
+    public $name = null;
+
+    /**
+     * Carousel's inner div
+     * @var \Ease\Html\Div 
+     */
+    public $inner = null;
+
+    /**
+     * Carousel's inner div
+     * @var \Ease\Html\OlTag 
+     */
+    public $indicators = null;
+
+    /**
+     * Which slide is active ?
+     * @var int 
+     */
+    public $active = null;
+
+    /**
+     * Twitter bootstrap Carousel
+     *  
+     * @url http://getbootstrap.com/javascript/#carousel
+     * @param string $name
+     * @param array $properties ['data-ride'=>'carousel'] means autorun
+     */
+    public function __construct($name = null, $properties = null) {
+        
+        parent::__construct(null, $properties);
+        $this->name = $this->setTagID($name);
+        $this->setTagClass('carousel slide');
+        $this->indicators = $this->addItem(new \Ease\Html\OlTag(null, ['class' => 'carousel-indicators']));
+        $this->inner = $this->addItem(new \Ease\Html\Div(null, ['class' => 'carousel-inner', 'role' => 'listbox']));
+    }
+
+    /**
+     * Carousel Slide
+     * 
+     * @param mixed|ImgTag $slide body Image or something else
+     * @param string $capHeading 
+     * @param string $caption
+     * @param boolean $default show slide by default
+     */
+    function addSlide($slide, $capHeading = '', $caption = '', $default = false) {
+        $item = new \Ease\Html\Div($slide, ['class' => 'item']);
+        if ($default) {
+            $item->addTagClass('active');
+        }
+
+        if ($capHeading || $caption) {
+            $cpt = $item->addItem(new \Ease\Html\Div(null, ['class' => 'carousel-caption']));
+            if ($capHeading) {
+                $cpt->addItem(new \Ease\Html\H4Tag($capHeading));
+            }
+            if ($caption) {
+                $cpt->addItem(new \Ease\Html\PTag($caption));
+            }
+        }
+        $to = $this->indicators->getItemsCount();
+        $indicator = new \Ease\Html\LiTag(null, ['data-target' => '#' . $this->name, 'data-slide-to' => $to]);
+        if ($default) {
+            $indicator->addTagClass('active');
+            $this->active = $to;
+        }
+        $this->indicators->addItem($indicator);
+        $this->inner->addItem($item);
+    }
+
+    /**
+     * Add Navigation buttons
+     */
+    function finalize() {
+        Part::twBootstrapize();
+        if (is_null($this->active)) { //We need one slide active
+            $this->indicators->getFirstPart()->setTagClass('active');
+            $this->inner->getFirstPart()->addTagClass('active');
+        }
+        $this->inner->addItem(
+                new \Ease\Html\ATag('#' . $this->getTagID(), [
+            new \Ease\Html\Span(null, ['class' => 'glyphicon glyphicon-chevron-left', 'aria-hidden' => 'true']),
+            new \Ease\Html\Span(_('Previous'), ['class' => 'sr-only'])
+                ], ['class' => 'left carousel-control', 'data-slide' => 'prev', 'role' => 'button']));
+        $this->inner->addItem(
+                new \Ease\Html\ATag('#' . $this->getTagID(), [
+            new \Ease\Html\Span(null, ['class' => 'glyphicon glyphicon-chevron-right', 'aria-hidden' => 'true']),
+            new \Ease\Html\Span(_('Next'), ['class' => 'sr-only'])
+                ], ['class' => 'right carousel-control', 'data-slide' => 'next', 'role' => 'button']));
+        if($this->getTagProperty('data-ride')!='carousel'){
+            $this->addJavaScript('$(\'#' . $this->name . '\').carousel();', null, true);
+        }
+//$this->includeJavaScript('js/bootstrap-carousel.js');
+    }
+
+}
