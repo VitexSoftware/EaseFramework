88342e60b118b6a3a8e2717763fae4f3d7d1518b
Vítězslav Dvořák <info@vitexsoftware.cz>
2017-12-06 21:09:07 +0100

    DateTime object as acceptable parameter for InputDate & InputDateTimeLocal

--- src/Ease/Html/InputDateTag.php
+++ src/Ease/Html/InputDateTag.php
@@ -13,15 +13,15 @@ class InputDateTag extends InputTag
     /**
      * The <input type="date"> is used for input fields that should contain a date.
      *
-     * @param string $name       name
-     * @param string $value      initial value
-     * @param array  $properties additional properties
+     * @param string       $name       input name
+     * @param string|\Date $value      initial value as string or DateTime 
+     * @param array        $properties additional properties
      */
     public function __construct($name, $value = null, $properties = [])
     {
         $properties['type']  = 'date';
-        $properties['value'] = $value;
+        $properties['value'] = is_object($value) ? $value->format('Y-m-d') : $value;
         $properties['name']  = $name;
-        parent::__construct($name, $value, $properties);
+        parent::__construct($name, $properties['value'], $properties);
     }
 }
--- src/Ease/Html/InputDateTimeLocalTag.php
+++ src/Ease/Html/InputDateTimeLocalTag.php
@@ -11,17 +11,17 @@ class InputDateTimeLocalTag extends InputTag
 {
 
     /**
-     * The <input type="date"> is used for input fields that should contain a
+     * The <input type="datetime-local"> is used for input fields that should contain a
      * date and time with no time zone.
      *
-     * @param string $name       name
-     * @param string $value      initial value
-     * @param array  $properties additional properties
+     * @param string           $name       name
+     * @param string|\DateTime $value      initial value as string or DateTime 
+     * @param array            $properties additional properties
      */
     public function __construct($name, $value = null, $properties = [])
     {
         $properties['type']  = 'datetime-local';
-        $properties['value'] = $value;
+        $properties['value'] = is_object($value) ? $value->format('c') : $value;
         $properties['name']  = $name;
         parent::__construct($name, $value, $properties);
     }
