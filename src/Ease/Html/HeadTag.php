c91cbdcaeb5e76b7078fa54bf156ec332d77a7bf
Cybervitexus <info@vitexsoftware.cz>
2019-05-13 12:25:53 +0200

    Better Scripts and Styles rendering

--- src/Ease/Html/HeadTag.php
+++ src/Ease/Html/HeadTag.php
@@ -5,7 +5,7 @@ namespace Ease\Html;
 /**
  * HTML webPage head class.
  *
- * @author Vitex <vitex@hippy.cz>
+ * @author Vítězslav Dvořák <info@vitexsoftware.cz>
  */
 class HeadTag extends PairTag
 {
@@ -79,57 +79,90 @@ class HeadTag extends PairTag
     }
 
     /**
+     * 
+     * @param array $scriptsArray
+     * @param string $divider use '' for optimized output
+     * 
+     * @return string
+     */
+    static public function getScriptsRendered(array $scriptsArray,
+                                              $divider = "\n")
+    {
+        $scriptsRendered = '';
+        ksort($scriptsArray, SORT_NUMERIC);
+        $scriptsInline   = [];
+        $scriptsIncluded = [];
+        $ODRStack        = [];
+        foreach ($scriptsArray as $script) {
+            $scriptType = $script[0];
+            $scriptBody = substr($script, 1);
+            switch ($scriptType) {
+                case '#':
+                    $scriptsIncluded[] = '<script src="'.$scriptBody.'"></script>';
+                    break;
+                case '@':
+                    $scriptsInline[]   = $scriptBody;
+                    break;
+                case '$':
+                    $ODRStack[]        = $scriptBody;
+                    break;
+            }
+        }
+
+        if ($scriptsIncluded) {
+            $scriptsIncluded .= $divider.implode($divider, $scriptsIncluded);
+        }
+
+        if ($scriptsInline) {
+            $scriptsIncluded .= $divider.self::jsEnclosure(implode($divider,
+                        $scriptsInline));
+        }
+
+        if (count($ODRStack)) {
+            $scriptsRendered .= $divider.
+                self::jsEnclosure(
+                    '$(document).ready(function () { '.implode($divider,
+                        $ODRStack).' });'
+            );
+        }
+        return $scriptsRendered;
+    }
+
+    /**
+     * Get included and inline Syles Fragment rendered
+     * 
+     * @param array  $stylesArray
+     * @param string $media
+     * @param string $divider use '' for optimized output
+     * 
+     * @return string
+     */
+    static public function getStylesRendered(array $stylesArray,
+                                             $media = 'screen', $divider = "\n")
+    {
+        $cascadeStyles         = [];
+        $cascadeStylesIncludes = [];
+        foreach ($stylesArray as $styleRes => $style) {
+            if ($styleRes == $style) {
+                $cascadeStylesIncludes[] = '<link href="'.$style.'" rel="stylesheet" type="text/css" media="'.$media.'" />';
+            } else {
+                $cascadeStyles[] = $style;
+            }
+        }
+        return implode($divider, $cascadeStylesIncludes).$divider.'<style>'.implode($divider,
+                $cascadeStyles).'</style>';
+    }
+
+    /**
      * Vykreslí hlavičku HTML stránky.
      */
     public function draw()
     {
-        if (isset($this->easeShared->cascadeStyles) && count($this->easeShared->cascadeStyles)) {
-            $cascadeStyles = [];
-            foreach ($this->easeShared->cascadeStyles as $StyleRes => $Style) {
-                if ($StyleRes == $Style) {
-                    $this->addItem('<link href="'.$Style.'" rel="stylesheet" type="text/css" media="'.'screen'.'" />');
-                } else {
-                    $cascadeStyles[] = $Style;
-                }
-            }
-            $this->addItem(
-                '<style>'.implode(
-                    '
-', $cascadeStyles
-                ).'</style>'
-            );
+        if (isset($this->easeShared->cascadeStyles) && !empty($this->easeShared->cascadeStyles)) {
+            $this->addItem(self::getStylesRendered($this->easeShared->cascadeStyles));
         }
-        if (isset($this->easeShared->javaScripts) && count($this->easeShared->javaScripts)) {
-            ksort($this->easeShared->javaScripts, SORT_NUMERIC);
-            $ODRStack = [];
-            foreach ($this->easeShared->javaScripts as $Script) {
-                $ScriptType = $Script[0];
-                $ScriptBody = substr($Script, 1);
-                switch ($ScriptType) {
-                    case '#':
-                        $this->addItem(
-                            '
-'.'<script src="'.$ScriptBody.'"></script>'
-                        );
-                        break;
-                    case '@':
-                        $this->addItem(self::jsEnclosure($ScriptBody));
-                        break;
-                    case '$':
-                        $ODRStack[] = $ScriptBody;
-                        break;
-                }
-            }
-            if (count($ODRStack)) {
-                $this->addItem(
-                    self::jsEnclosure(
-                        '$(document).ready(function () { '.implode(
-                            '
-', $ODRStack
-                        ).' });'
-                    )
-                );
-            }
+        if (isset($this->easeShared->javaScripts) && !empty($this->easeShared->javaScripts)) {
+            $this->addItem(self::getScriptsRendered($this->easeShared->javaScripts));
         }
         parent::draw();
     }
