3ea55de3658ccb31b44b369c64e2c56b8b665132
Vítězslav Dvořák <info@vitexsoftware.cz>
2019-08-30 20:32:45 +0200

    getRequestValue() is static

--- src/Ease/Page.php
+++ src/Ease/Page.php
@@ -325,7 +325,7 @@ class Page extends Container
      *
      * @return mixed
      */
-    public function getRequestValue($field, $sanitizeAs = null)
+    public static function getRequestValue($field, $sanitizeAs = null)
     {
         $value = null;
         if (isset($_REQUEST[$field])) {
